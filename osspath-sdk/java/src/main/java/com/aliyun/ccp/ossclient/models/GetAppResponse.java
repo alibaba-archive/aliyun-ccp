0e0d36b27baab67e74db965bcaca7910e4f1b8de
Jackson Tian <shyvo1987@gmail.com>
2019-10-30 15:59:48 +0800

    regenerate for java

---
+++ osspath-sdk/java/src/main/java/com/aliyun/ccp/ossclient/models/GetAppResponse.java
@@ -0,0 +1,53 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ccp.ossclient.models;
+
+import com.aliyun.tea.*;
+
+public class GetAppResponse extends TeaModel {
+    @NameInMap("requestId")
+    public String requestId;
+
+    @NameInMap("ali_owner_id")
+    public String aliOwnerId;
+
+    @NameInMap("app_id")
+    public String appId;
+
+    @NameInMap("app_name")
+    public String appName;
+
+    @NameInMap("app_secret")
+    public String appSecret;
+
+    @NameInMap("created_at")
+    public String createdAt;
+
+    @NameInMap("description")
+    public String description;
+
+    @NameInMap("logo")
+    public String logo;
+
+    @NameInMap("provider")
+    public String provider;
+
+    @NameInMap("redirect_uri")
+    public String redirectUri;
+
+    @NameInMap("scope")
+    public String[] scope;
+
+    @NameInMap("screenshots")
+    public String[] screenshots;
+
+    @NameInMap("stage")
+    public String stage;
+
+    @NameInMap("type")
+    public String type;
+
+    @NameInMap("updated_at")
+    public String updatedAt;
+
+}
+
