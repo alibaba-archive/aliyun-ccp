3e0856f206836bdcdb97b6ada5d01040da3c3d2b
liaoyustudent <wb-ly617236@alibaba-inc.com>
2019-11-22 15:48:26 +0800

    ccp release version 0.0.1

---
+++ ccppath-sdk/cs/ChangeLog.md
@@ -0,0 +1,2 @@
+### 2019-11-22 Version 0.0.1
+* First release.
---
+++ osspath-sdk/cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aliyun-sdk-ccp-oss-client")]
+[assembly: AssemblyDescription("aliyun ccp oss client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alibaba Cloud, Inc")]
+[assembly: AssemblyProduct("aliyun ccp oss client")]
+[assembly: AssemblyCopyright("©2009-2019 Alibaba Cloud")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("20f9b031-f768-474c-97c2-642dbde6d778")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
