6598d027106177c3e775560e119e9863f216146d
liaoyustudent <wb-ly617236@alibaba-inc.com>
2019-11-22 15:48:26 +0800

    ccp release version 0.0.1

---
+++ ccppath-sdk/cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("aliyun-sdk-ccp-client")]
+[assembly: AssemblyDescription("aliyun ccp client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Alibaba Cloud, Inc")]
+[assembly: AssemblyProduct("aliyun ccp client")]
+[assembly: AssemblyCopyright("©2009-2019 Alibaba Cloud")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b2277c38-1052-4a61-9e98-e4cfa2151966")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
