f09cfc93250ea5c015201d7776fa3af79d6964d5
sonny <wb-szq493230@alibaba-inc.com>
2020-02-15 19:49:26 +0800

    add file

---
+++ osspath-sdk/java/src/main/java/com/aliyun/ccp/ossclient/models/SharePermissionPolicy.java
@@ -0,0 +1,24 @@
+// This file is auto-generated, don't edit it. Thanks.
+package com.aliyun.ccp.ossclient.models;
+
+import com.aliyun.tea.*;
+
+public class SharePermissionPolicy extends TeaModel {
+    @NameInMap("file_path")
+    public String filePath;
+
+    @NameInMap("permission_inheritable")
+    public boolean permissionInheritable;
+
+    @NameInMap("permission_list")
+    public String[] permissionList;
+
+    @NameInMap("permission_type")
+    public String permissionType;
+
+    public static SharePermissionPolicy build(java.util.Map<String, ?> map) throws Exception {
+        SharePermissionPolicy self = new SharePermissionPolicy();
+        return TeaModel.build(map, self);
+    }
+
+}
