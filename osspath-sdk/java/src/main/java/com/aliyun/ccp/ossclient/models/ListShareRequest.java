3f1e3bea055d054fb2b0ddfb4a2794b7b8ef7966
wenzuochao <wb-wzc505509@alibaba-inc.com>
2020-02-19 18:57:37 +0800

    update oss

--- osspath-sdk/java/src/main/java/com/aliyun/ccp/ossclient/models/ListShareRequest.java
+++ osspath-sdk/java/src/main/java/com/aliyun/ccp/ossclient/models/ListShareRequest.java
@@ -7,6 +7,10 @@ public class ListShareRequest extends TeaModel {
     @NameInMap("creator")
     public String creator;
 
+    @NameInMap("drive_id")
+    @Validation(pattern = "[0-9]+")
+    public String driveId;
+
     @NameInMap("limit")
     @Validation(required = true)
     public Integer limit;
@@ -16,6 +20,9 @@ public class ListShareRequest extends TeaModel {
 
     @NameInMap("owner")
     public String owner;
+
+    @NameInMap("share_file_path")
+    public String shareFilePath;
 
     public static ListShareRequest build(java.util.Map<String, ?> map) throws Exception {
         ListShareRequest self = new ListShareRequest();
